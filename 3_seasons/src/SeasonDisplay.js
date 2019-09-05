17e90ea151c5b9cd8da6af7d3b4213302db100c8
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-09-05 10:02:36 +1000

    Add function to get season from latitude in SeasonDisplay

--- 3_seasons/src/SeasonDisplay.js
+++ 3_seasons/src/SeasonDisplay.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+
+// Helper functions in the main body of the file are ok for functional components
+const getSeason = (lat, month) => {
+    if (month>2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat > 0 ? 'winter' : 'summer';
+    }
+};
+
+
 const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
     return (
-        <div>SeasonDisplay Lat: {props.lat}</div>
+        <div>SeasonDisplay: {season}</div>
     );
 };
 
