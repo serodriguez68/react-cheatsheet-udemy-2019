e68252d7c2c1b17faecb4709e31ea30e942b4497
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-09-11 20:08:09 +1000

    Create App component

--- 5_videos/src/components/App.js
+++ 5_videos/src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
     render() {
-        return <div>Hello from App</div>;
+        return (
+            <div className='ui container'>
+                <SearchBar />
+            </div>
+        );
     }
 }
 
