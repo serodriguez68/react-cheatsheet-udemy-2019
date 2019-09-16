6b2fc0f5e137e35c831e09059527affa053e6bd3
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-09-16 21:49:40 +1000

    Wire up Video Detail

--- 5_videos/src/components/App.js
+++ 5_videos/src/components/App.js
@@ -1,27 +1,44 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from "../apis/youtube";
+
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null};
 
+    // Default search when the app is initially loaded
+    componentDidMount() {
+        this.fetchVideos('buildings');
+    }
+
     fetchVideos = async (term) => {
-        console.log(`Fetching videos for "${term}"`);
         const response = await youtube.get('/search', {params: {q: term}});
-        console.log(response.data.items);
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     };
 
     onVideoSelect = (video) => {
-      console.log('From app', video);
+      this.setState({selectedVideo: video});
     };
 
     render() {
         return (
             <div className='ui container'>
                 <SearchBar onSearchSubmit={this.fetchVideos}/>
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className='eleven wide column'>
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className='five wide column'>
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- 5_videos/src/components/VideoItem.js
+++ 5_videos/src/components/VideoItem.js
@@ -4,7 +4,9 @@ import './VideoItem.css';
 const VideoItem = ({ video, onVideoSelect }) => {
     return(
         <div className="video-item item" onClick={ () => {onVideoSelect(video)} }>
-            <img className="ui image" src={video.snippet.thumbnails.medium.url} alt=""/>
+            <img className="ui image"
+                 src={video.snippet.thumbnails.medium.url}
+                 alt={video.snippet.title}/>
             <div className="content">
                 <div className="header">
                     {video.snippet.title}
