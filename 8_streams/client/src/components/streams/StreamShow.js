e779b1811246831e2a3fd9a75313ef72ed829ab5
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-10-19 15:09:26 +1100

    Connect streaming server with StremShow component

--- 8_streams/client/src/components/streams/StreamShow.js
+++ 8_streams/client/src/components/streams/StreamShow.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import flv from 'flv.js';
 import {connect} from 'react-redux';
 import {fetchStream} from "../../actions";
 
 
 class StreamShow extends React.Component{
+
+    constructor(props) {
+        super(props);
+        // Create a ref that points to the video element
+        this.videoRef = React.createRef();
+    }
+
     componentDidMount() {
         // IMPORTANT: components that rely on wildcard navigation must be self-sufficient and
         // fetch the data that they need.  We cannot rely on the data being present on the redux store.
         const urlId = this.props.match.params.id;
         this.props.fetchStream(urlId);
+        this.buildPlayer(); // Try to build player during the initial render
+    }
+
+    //Try to build player when the component is updated
+    componentDidUpdate() {
+        this.buildPlayer();
+    }
+
+    // When we navigate out of this component, we need to do clean-up by stopping the streaming.
+    componentWillUnmount() {
+        this.player.destroy(); // Stops streaming video and detach from html video element
+    }
+
+    // Initialize the video player
+    buildPlayer() {
+        // Do nothing if there is a player already or we have not fetched the stream yet
+        if (this.player || !this.props.stream){
+            return;
+        }
+        this.player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${this.props.stream.id}.flv`
+        });
+        this.player.attachMediaElement(this.videoRef.current);
+        this.player.load();
     }
 
     render() {
@@ -19,6 +52,7 @@ class StreamShow extends React.Component{
         const {title, description} = this.props.stream;
         return (
             <div>
+                <video ref={this.videoRef} style={{width: '100%'}} controls={true} />
                 <h1>{title}</h1>
                 <h5>{description}</h5>
             </div>
